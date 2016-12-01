70f03a9e8430b27e60267da87e3d4f944010f543
Que P <tinhbanbonphuong126@gmail.com>
2016-12-01 17:51:17 +0700

    Final Table data sort

--- Tech/Table and sort/js/script.js
+++ Tech/Table and sort/js/script.js
@@ -1,11 +1,23 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "ready!" );
-    $('#myTable').DataTable();
+    var t = $('#myTable').DataTable( {
+        "columnDefs": [ {
+            "searchable": false,
+            "orderable": false,
+            "targets": 0
+        } ],
+        "order": [[ 1, 'asc' ]]
+    } );
+
+    t.on( 'order.dt search.dt', function () {
+        t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+            cell.innerHTML = i+1;
+        } );
+    } ).draw();
+
+    });
 
 
 
 
-
-
-});
