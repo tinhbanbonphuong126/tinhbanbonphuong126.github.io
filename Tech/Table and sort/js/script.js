57c3d9d1bf371803919eae1bbecebd776ffe9fdd
Que P <tinhbanbonphuong126@gmail.com>
2016-12-02 02:44:53 +0700

    Home update

---
+++ Tech/Table and sort/js/script.js
@@ -0,0 +1,23 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var t = $('#myTable').DataTable( {
+        "columnDefs": [ {
+            "searchable": false,
+            "orderable": false,
+            "targets": 0
+        } ],
+        "order": [[ 1, 'asc' ]]
+    } );
+
+    t.on( 'order.dt search.dt', function () {
+        t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+            cell.innerHTML = i+1;
+        } );
+    } ).draw();
+
+    });
+
+
+
+
