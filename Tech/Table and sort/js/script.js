03d4ed214344bc5b24546c65396b6146e5b0324b
Que P <tinhbanbonphuong126@gmail.com>
2016-12-01 13:13:16 +0700

    Update

---
+++ Tech/Table and sort/js/script.js
@@ -0,0 +1,17 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    //selector row: table tbody tr
+    //$('table tbody tr').hover(function(){
+    //    console.log('hover ok');
+    //    $('.table tbody tr .show-option').show();
+    //})
+    //
+    //
+
+
+
+
+
+});
