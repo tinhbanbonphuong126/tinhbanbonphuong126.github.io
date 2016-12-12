b393b5a4aad6cf4267c3420d2ef9fefec2a8b29f
Que P <tinhbanbonphuong126@gmail.com>
2016-12-12 17:02:13 +0700

    update
    Update

---
+++ PHP/Ex-PHP/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Giải phương trình bậc nhất</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<?php
+$result = '';
+?>
+<h1>Giải phương trình bậc nhất</h1>
+<form method="post" action="">
+    <input type="text" style="width: 20px" name="a" value=""/>x
+    +
+    <input type="text" style="width: 20px" name="b" value=""/> = 0
+
+    <input type="submit" name="Tinh" value="Tinh">
+</form>
+<?php echo $result; ?>
+</body>
+</html>
